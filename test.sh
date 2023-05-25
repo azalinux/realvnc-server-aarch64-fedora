c7cdc8435174ece5f01f010e187c780fb4562091
Azalinux <52433182+azalinux@users.noreply.github.com>
2023-05-26 08:58:21 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,26 +3,22 @@ set -e
 
 sudo dnf -y install libxcrypt-compat xterm
 
-if [ ! -d /opt/vc ]; then
-echo "Creating /opt/vc directory..."
-sudo mkdir /opt/vc
-echo "/opt/vc directory created."
-echo "Running sudo tar xvf pifirmware_libs.tar.gz -C /opt/vc/..."
-sudo tar xvf pifirmware_libs.tar.gz -C /opt/vc/
-echo "sudo tar xvf pifirmware_libs.tar.gz -C /opt/vc/ completed."
+if [ ! -d "/opt/vc" ]; then
+  sudo mkdir -p /opt/vc
+  sudo tar xvf pifirmware_libs.tar.gz -C /opt/vc/
 else
-echo "/opt/vc directory already exists."
+  echo "/opt/vc exists, moving on"
 fi
 
-if [ ! -d /opt/vc/lib/libvcos.so /usr/lib64/libvcos.so.0 /opt/vc/lib/libvchiq_arm.so /usr/lib64/libvchiq_arm.so.0 /opt/vc/lib/libbcm_host.so /usr/lib64/libbcm_host.so.0 ]; then
-echo "Creating symlinks..."
-sudo ln -s /opt/vc/lib/libvcos.so /usr/lib64/libvcos.so.0
-sudo ln -s /opt/vc/lib/libvchiq_arm.so /usr/lib64/libvchiq_arm.so.0
-sudo ln -s /opt/vc/lib/libbcm_host.so /usr/lib64/libbcm_host.so.0
-echo "Symlinks created."
+if [ ! -f "/opt/vc/lib/libvcos.so" ] || [ ! -f "/usr/lib64/libvcos.so.0" ] || [ ! -f "/opt/vc/lib/libvchiq_arm.so" ] || [ ! -f "/usr/lib64/libvchiq_arm.so.0" ] || [ ! -f "/opt/vc/lib/libbcm_host.so" ] || [ ! -f "/usr/lib64/libbcm_host.so.0" ]; then
+  sudo ln -s /opt/vc/lib/libvcos.so /usr/lib64/libvcos.so.0
+  sudo ln -s /opt/vc/lib/libvchiq_arm.so /usr/lib64/libvchiq_arm.so.0
+  sudo ln -s /opt/vc/lib/libbcm_host.so /usr/lib64/libbcm_host.so.0
 else
-echo "Symlinks already exist."
+  echo "Moving on"
 fi
+
+
 
 sudo tar xvf data.tar.xz -C /
 
